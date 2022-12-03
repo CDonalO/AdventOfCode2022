760987cb2862fba1132867e7b4951885a409e991
Cordell O'Leary <cordelloleary@gmail.com>
2022-12-03 23:12:59 +1300

    solution

--- Day 1/main.c
+++ Day 1/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int highest_sum = 0;
+    int sum = 0;
+    char buff[255];
+    while (fgets(buff,255,stdin)!=NULL){
+        if(strcmp(buff,"\n") == 0){
+            if(sum > highest_sum) {
+                highest_sum = sum;
+
+                sum = 0;
+            }
+        }
+        sum += atoi(buff);
+    }
+    printf("%d\n",highest_sum);
     return 0;
 }
