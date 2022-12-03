31919765041281b838affb7a5dcc07cc79f76fee
Cordell O'Leary <cordelloleary@gmail.com>
2022-12-03 23:56:56 +1300

    added part 2 solution

--- Day 1/main.c
+++ Day 1/main.c
@@ -2,20 +2,40 @@
 #include <string.h>
 #include <stdlib.h>
 
+int cmpfunc (const void * a, const void * b) {
+    return ( *(int*)b - *(int*)a );
+}
+int getSumOfN(int* arr,int count){
+    int sum = 0;
+    for(int i =0;i<count;i++){
+        int num = *(arr+i);
+        sum += num;
+    }
+    return sum;
+}
+
 int main() {
     int highest_sum = 0;
     int sum = 0;
     char buff[255];
+    int count = 0;
+    int* elves = malloc(sizeof(int));
     while (fgets(buff,255,stdin)!=NULL){
         if(strcmp(buff,"\n") == 0){
             if(sum > highest_sum) {
                 highest_sum = sum;
-
-                sum = 0;
             }
+            *(elves+count) = sum;
+            count++;
+            elves = realloc(elves,(sizeof(int)*(count+1)));
+            sum = 0;
         }
         sum += atoi(buff);
     }
-    printf("%d\n",highest_sum);
+
+    qsort(elves,count,sizeof(int),cmpfunc);
+    int sumN = getSumOfN(elves,3);
+    printf("%d\n",sumN);
+    printf("%d",highest_sum);
     return 0;
 }
