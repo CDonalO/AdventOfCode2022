8eda8363a2b316b9506ac48e29af91868ef4fd93
Cordell O'Leary <cordelloleary@gmail.com>
2022-12-04 23:58:09 +1300

    added part 2 solution

--- Day 2/main.c
+++ Day 2/main.c
@@ -13,7 +13,6 @@ char decryptSign(char a){
 }
 
 int psrScore(char a,char b){
-    b = decryptSign(b);
     int score = 0;
     if (b == 'A'){
         score += 1;
@@ -31,13 +30,42 @@ int psrScore(char a,char b){
     return score;
 }
 
+char fixedSign(char result,char sign){
+    if(result == 'Y'){
+        return sign;
+    } else if (result == 'X'){
+        if(sign == 'A'){
+            return 'C';
+        } else if (sign == 'B'){
+            return 'A';
+        } else if (sign == 'C'){
+            return 'B';
+        }
+    } else if (result == 'Z'){
+        if(sign == 'A'){
+            return 'B';
+        } else if (sign == 'B'){
+            return 'C';
+        } else if (sign == 'C'){
+            return 'A';
+        }
+    }
+    return ' ';
+}
+
 int main() {
     int points = 0;
+    int oldPoints = 0;
     char buff[255];
+    char oldSign;
     while (fgets(buff,255,stdin)!=NULL){
-        points += psrScore(buff[0],buff[2]);
+//        Part 1 stuff
+        oldSign = decryptSign(buff[2]);
+        oldPoints += psrScore(buff[0],oldSign);
+//        Part 2 stuff
+        points += psrScore(buff[0], fixedSign(buff[2],buff[0]));
     }
-
+    printf("%d\n",oldPoints);
     printf("%d",points);
     return 0;
 }
